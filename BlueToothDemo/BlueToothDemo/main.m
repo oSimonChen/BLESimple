6a6181c368a91c594a6f5e7a305a35b0f5964cda
chenxingwang <xing_wang_2012@hotmail.com>
2017-10-27 09:27:48 +0800

    init project

---
+++ BlueToothDemo/BlueToothDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlueToothDemo
+//
+//  Created by chenxingwang on 2017/10/26.
+//  Copyright © 2017年 chenxingwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
