a6c2dceaf176800729a4f3bcb2d8f255dcd2c111
chenxingwang <xing_wang_2012@hotmail.com>
2017-10-27 12:52:12 +0800

    # target

---
+++ BlueToothDemo/peripheralModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  peripheralModel
+//
+//  Created by chenxingwang on 2017/10/27.
+//  Copyright © 2017年 chenxingwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlueToothDemo/peripheralModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  peripheralModel
+//
+//  Created by chenxingwang on 2017/10/27.
+//  Copyright © 2017年 chenxingwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
